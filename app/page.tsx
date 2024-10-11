0ec31d7dcb1b433f339877883c033fc6340f28c7
caldayham <caldayham@gmail.com>
2024-10-10 22:14:39 -0700

    new updates to main pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,33 @@ export default function Home() {
   return (
     <div>
       <h1>design.caldayham.com</h1>
-      <br/>
+      <br />
 
       Your hardscape dream, pulled into reality.
-      <br/>
-      The hardscape catalase.
+      <br />
+
+      You have spent your life creating value in your career, building an amazing family, buying a home, and transforming it into a space that stores memories,
+      <br />
+      <br />
+
+      provides solitude, and promotes how you want to live your life.
+
+      But you don't feel connected to your outdoor space.
+
+      Perhaps it's a product of someone elses mind, someone with different values and tastes.
+      Perhaps it's functionality has been slowly scraped away by father time.
+      Perhaps you have changed and a space you once loved feels out of place now.
+
+      Whatever the reason, you're reading this because it is time for a change.
+
+      <br />
+
+      Navigating the tens of thousands of materials, and even more exponential possible design combinations to uncover your perfect installation is both an extremely difficult,
+      and extremely rewarding endeavour.
+
+      My purpose is to bridge this gap for you with clarity forged from experience.
+
+      If you're ready to meet me, discuss your ideas, and learn a little about this process, book a consultation below.
     </div>
   );
 }
